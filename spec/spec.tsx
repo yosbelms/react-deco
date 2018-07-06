e008f2b48f4d2c24949f23e79f81aaeab85fde0e
Yosbel Marín <yosbel@yosbel.com>
2018-07-05 23:50:47 -0400

    add pure zone

--- spec/spec.tsx
+++ spec/spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { If, Map } from '../src'
+import { If, Map, Pure } from '../src'
 import { mount } from 'enzyme'
 
 function RenderIf({ test }: { test: any }) {
@@ -26,22 +26,26 @@ describe('If', () => {
     const wrapper = mount(<RenderIf test={true} />)
     expect(wrapper.find('.then').length).toBe(1)
     expect(wrapper.find('.else').length).toBe(0)
+    wrapper.unmount()
   })
 
   it('should render `else` if `test` is falsy', () => {
     const wrapper = mount(<RenderIf test={false} />)
     expect(wrapper.find('.then').length).toBe(0)
     expect(wrapper.find('.else').length).toBe(1)
+    wrapper.unmount()
   })
 
   it('should accept a components in `then` and `else`', () => {
     const wrapper = mount(<RenderIfWithComponents test={true} />)
     expect(wrapper.find('.then').length).toBe(1)
     expect(wrapper.find('.else').length).toBe(0)
+    wrapper.unmount()
 
     const wrapperwc = mount(<RenderIfWithComponents test={false} />)
     expect(wrapperwc.find('.then').length).toBe(0)
     expect(wrapperwc.find('.else').length).toBe(1)
+    wrapperwc.unmount()
   })
 
 })
@@ -50,11 +54,65 @@ describe('If', () => {
 describe('Map', () => {
 
   it('should work', () => {
-    const wrapper = mount(<Map target={[1, 2, 3]} with={(item: number) => 
+    const wrapper = mount(<Map target={[1, 2, 3]} with={(item: number) =>
       <div key={item} className={'item'} />
-    }/>)
+    } />)
 
     expect(wrapper.find('.item').length).toBe(3)
+    wrapper.unmount()
+  })
+
+})
+
+function RenderPure({ test }: { test: any }) {
+  const rand = Math.random()
+  return (
+    <Pure test={test} render={
+      <div className='div' data-rand={rand} />
+    } />
+  )
+}
+
+function RenderPurePassingProps({ test }: { test: any }) {
+  return (
+    <Pure test={test} render={(cls) =>
+      <div className={cls} />
+    } />
+  )
+}
+
+describe('Pure', () => {
+
+  it('should update only if `test` id changed', () => {
+    let wrapper = mount(<RenderPure test={['a', 'b']} />)
+
+    let div = wrapper.find('.div').getDOMNode()
+    expect(div).toBeTruthy()
+
+    let rand = div.getAttribute('data-rand')
+
+    // rerender
+    wrapper.setProps({ test: ['a', 'b'] })
+
+    div = wrapper.find('.div').getDOMNode()
+    expect(div.getAttribute('data-rand')).toBe(rand)
+
+    // rerender
+    wrapper.setProps({ test: ['b', 'a'] })
+
+    div = wrapper.find('.div').getDOMNode()
+    expect(div.getAttribute('data-rand')).not.toBe(rand)
+
+    wrapper.unmount()
+  })
+
+  it('should pass tested values to `render` if it is a function', () => {
+    let wrapper = mount(<RenderPurePassingProps test={'div'} />)
+
+    let res = wrapper.find('.div')
+    expect(res.length).toBe(1)
+
+    wrapper.unmount()
   })
 
 })
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { ReactElement, createElement } from 'react'
 import * as PropTypes from 'prop-types'
+import * as shallowEqual from 'fbjs/lib/shallowEqual'
 
 function isFunction(v) {
   return typeof v === 'function'
@@ -10,8 +11,8 @@ function defaultToNull(v: any) {
   return v === void 0 ? null : v
 }
 
-function toElement(c) {
-  return isFunction(c) ? createElement(c) : defaultToNull(c)
+function toElement(c, data = void 0) {
+  return defaultToNull(isFunction(c) ? c(data) : c)
 }
 
 /**
@@ -46,4 +47,41 @@ export function Map<T>({ target, 'with': _with }: {
 (Map as any).propTypes = {
   target: PropTypes.any.isRequired,
   with: PropTypes.func.isRequired
+}
+
+/**
+ * Re-render the content of the `render` prop if the value of `test` is not shallow-equal
+ * to the `test` value provided in the previous render.
+ */
+export class Pure extends React.Component<
+  {
+    test: any
+    render: any
+  }
+  > {
+
+  previousTest: any = null
+
+  constructor(props, context) {
+    super(props, context)
+    this.previousTest = props.test
+  }
+
+  shouldComponentUpdate(nextProps, _) {
+    const nextTest = nextProps.test
+    const should = !shallowEqual(this.previousTest, nextTest)
+    if (should) {
+      this.previousTest = nextTest
+    }
+    return should
+  }
+
+  render() {
+    return toElement(this.props.render, this.previousTest)
+  }
+}
+
+(Pure as any).propTypes = {
+  test: PropTypes.any.isRequired,
+  render: PropTypes.any.isRequired
 }
